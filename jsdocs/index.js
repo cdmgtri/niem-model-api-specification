cc6c790dd60756bb357a28a0dd4fb0eeb618a6a2
CDM <>
2018-11-28 22:55:49 -0500

    Updated export of model JSDocs

--- jsdocs/index.js
+++ jsdocs/index.js
@@ -1,50 +1,20 @@
 
-// let modelTypes = require("../node_modules/niem-model-schema/jsdocs/index");
+let { NIEMModel, NIEMVersion } = require("niem-model-schema");
 
-
-/**
-  * A data type representing the NIEM data model, an IEPD, an EIEM, or another kind of NIEM-based data model.
-  * @typedef {Object} NIEMModel
-  *
-  * @property {string} [name] - A name of a model.
-  * @property {string} [summary] - A definition or summary statement of a model.
-  * @property {string} [description] - A detailed description of a model.
-  * @property {enum} [kind] - A kind of model.
-  * @property {string} [source] - An authoritative source for the model.
-  * @property {string} [contactInfo] - A set of contact information for the model.
-  * @property {string} [website] - A URL for an informational website about this model.
-  * @property {NIEMVersion[]} [versions] - A set of versions that belong to this model.
-  */
-
-
-/**
-  * A data type for a specific version or release of a model.
-  * @typedef {Object} NIEMVersion
-  *
-  * @property {string} [modelName] - A name of the model to which this version belongs.
-  * @property {string} [version] - A version number.
-  * @property {string} [draft] - A sub-version number, such as a pre-release identifier.
-  * @property {string} [baseNIEM] - A NIEM release that this resource is based on.
-  * @property {string} [uri] - A target namespace or other ID for this model version.
-  * @property {string} [changeSummary] - A summary of the changes from the previous version, if applicable.
-  * @property {string} [location] - A URL for the model version folder.
-  * @property {array} [conformanceTargets] - One or more classes of artifact to which this artifact conforms.
-  * @property {object} [more] - Any additional metadata to be captured.
-  */
 
 /**
   * Represents a undefined object
-  * @typedef {Object} NIEMModelResponse
+  * @typedef {Object} NIEMModelResponseType
   *
   * @property {string} [$schema] -
   * @property {NIEMModel} [data] - A data type representing the NIEM data model, an IEPD, an EIEM, or another kind of NIEM-based data model.
-  * @property {NIEMModelLinks} [links] - A data type for metadata about model-related resources.
+  * @property {NIEMModelLinksType} [links] - A data type for metadata about model-related resources.
   */
 
 
 /**
   * A data type for metadata about model-related resources.
-  * @typedef {Object} NIEMModelLinks
+  * @typedef {Object} NIEMModelLinksType
   *
   * @property {string} [id] - An identifier for a model.
   * @property {string} [href] - A URI for the target resource.
@@ -54,17 +24,17 @@
 
 /**
   * Represents a undefined object
-  * @typedef {Object} NIEMVersionResponse
+  * @typedef {Object} NIEMVersionResponseType
   *
   * @property {string} [$schema] -
   * @property {NIEMVersion} [data] - A data type for a specific version or release of a model.
-  * @property {NIEMVersionLinks} [links] - A data type for metadata about version-related resources.
+  * @property {NIEMVersionLinksType} [links] - A data type for metadata about version-related resources.
   */
 
 
 /**
   * A data type for metadata about version-related resources.
-  * @typedef {Object} NIEMVersionLinks
+  * @typedef {Object} NIEMVersionLinksType
   *
   * @property {string} [href] - A URI for the target resource.
   * @property {string} [zip] - The location of the zip file for this project.
@@ -93,3 +63,26 @@
   * @property {integer} [count] -
   * @property {string} [href] - A URI for the target resource.
   */
+
+
+/** @type {NIEMModelLinksType} */
+let NIEMModelLinks = {};
+
+/** @type {NIEMModelResponseType} */
+let NIEMModelResponse = {};
+
+/** @type {NIEMVersionLinksType} */
+let NIEMVersionLinks = {};
+
+/** @type {NIEMVersionResponseType} */
+let NIEMVersionResponse = {};
+
+module.exports = {
+  NIEMModel,
+  NIEMModelLinks,
+  NIEMModelResponse,
+
+  NIEMVersion,
+  NIEMVersionLinks,
+  NIEMVersionResponse,
+}
